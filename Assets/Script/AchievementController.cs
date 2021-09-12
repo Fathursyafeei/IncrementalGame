6417082482ef7493ab2feb2032f4de8057af84ad
Fathursyafeei <mufatraba@gmail.com>
2021-09-12 15:32:16 +0700

    first commit

---
+++ Assets/Script/AchievementController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchievementController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private static AchievementController _instance = null; //Mirip seperti pada GameManager, fungsinya adalah membuat sistem singleton untuk memudahkan pemanggilan script yang bersifat manager dari script lain
+    public static AchievementController Instance  
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<AchievementController>();
+            }
+            return _instance;
+        }
+    }
+
+    [SerializeField] private Transform _popUpTransform;
+    [SerializeField] private Text _popUpText;
+    [SerializeField] private float _popUpShowDuration = 3f;
+    [SerializeField] private List<AchievementData> _achievementList;
+
+    private float _popUpShowDurationCounter;
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        if (_popUpShowDurationCounter > 0)
+        {
+            _popUpShowDurationCounter -= Time.unscaledDeltaTime; // Kurangi durasi ketika pop up durasi lebih 
+            _popUpTransform.localScale = Vector3.LerpUnclamped(_popUpTransform.localScale, Vector3.one, 0.5f); // Kurangi durasi ketika pop up durasi lebih dari 0
+        }
+        else
+        {
+            _popUpTransform.localScale = Vector2.LerpUnclamped(_popUpTransform.localScale, Vector3.right, 0.5f);
+        }
+
+    }
+
+    public void UnlockAchievement(AchievementType type, string value)
+    {
+        AchievementData achievement = _achievementList.Find(a => a.Type == type && a.Value == value); // Mencari data achievement
+        if (achievement != null && !achievement.IsUnlocked)
+        {
+            achievement.IsUnlocked = true;
+            ShowAchivementPopUp(achievement);
+        }
+
+    }
+
+
+    private void ShowAchivementPopUp(AchievementData achievement)
+    {
+        _popUpText.text = achievement.Title;
+        _popUpShowDurationCounter = _popUpShowDuration;
+        _popUpTransform.localScale = Vector2.right;
+    }
+}
+
+[System.Serializable] // System.Serializable digunakan agar object dari script bisa di-serialize dan bisa di-inputkan dari Inspector, jika tidak terdapat ini, maka variable tidak akan muncul di inspector
+
+public class AchievementData
+{
+    public string Title;
+    public AchievementType Type;
+    public string Value;
+    public bool IsUnlocked;
+}
+
+public enum AchievementType
+{
+    UnlockResource
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance = null;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+    public double TotalGold { get; internal set; }
+
+    [Range (0f, 1f)] // fungsi [Range (min, max)] ialah menjaga value agar tetap berada di antara min dan max-nya 
+    public float AutoCollectPercentage = 0.1f;
+    public ResourceConfig[] ResourcesConfigs;
+    public Sprite[] ResourcesSprites;
+
+    public Transform ResourcesParent;
+    public ResourceController ResourcePrefab;
+    public TapText TapTextPrefab;
+
+    public Transform CoinIcon;
+    public Text GoldInfo;
+    public Text AutoCollectInfo;
+
+    private List<ResourceController> _activeResources = new List<ResourceController>();
+
+    private List<TapText> _tapTextPool = new List<TapText>();
+
+    private float _collectSecond;
+    //private double _totalGold;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        AddAllResource(); 
+    }
+
+    private void AddAllResource()
+    {
+        bool showResources = true;
+
+        foreach (ResourceConfig config in ResourcesConfigs)
+        {
+            GameObject obj = Instantiate(ResourcePrefab.gameObject, ResourcesParent, false);
+            ResourceController resource = obj.GetComponent<ResourceController>();
+
+            resource.SetConfig(config);
+
+            obj.gameObject.SetActive(showResources);
+
+            if (showResources && !resource.IsUnlocked)
+            {
+                showResources = false;
+            }
+
+            _activeResources.Add(resource);
+        }
+    }
+
+    public void ShowNextResource()
+    {
+        foreach (ResourceController resource in _activeResources)
+        {
+            if (!resource.gameObject.activeSelf)
+            {
+                resource.gameObject.SetActive(true);
+                break;
+            }
+        }
+    }
+
+    private void CollectPerSecond()
+    {
+        double output = 0;
+        foreach (ResourceController resource in _activeResources)
+        {
+            if (resource.IsUnlocked)
+            {
+                output += resource.GetOutput();
+            }
+        }
+        output *= AutoCollectPercentage;
+        AutoCollectInfo.text = $"Auto Collect: { output.ToString("F1") } / second"; // Fungsi ToString("F1") ialah mebulatkan angka  jadi desimal yang memiliki 1 angka di belangkang koma
+
+        AddGold(output);
+    }
+
+    public void AddGold(double value)
+    {
+        TotalGold += value;
+        GoldInfo.text = $"Gold: { TotalGold.ToString("0") }";
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        _collectSecond += Time.unscaledDeltaTime; // Fungsi untuk selalu mengeksekusi Collect Per Second setiap detik.
+        if(_collectSecond >= 1f)
+        {
+            CollectPerSecond();
+            _collectSecond = 0f;
+        }
+
+        CheckResourceCost();
+
+        CoinIcon.transform.localScale = Vector3.LerpUnclamped(CoinIcon.transform.localScale, Vector3.one * 2f, 0.15f);
+        CoinIcon.transform.Rotate(0f, 0f, Time.deltaTime * -100f);
+    }
+
+    public void CollectByTap(Vector3 tapPosition, Transform parent)
+    {
+        double output = 0;
+        foreach(ResourceController resource in _activeResources)
+        {
+            if (resource.IsUnlocked)
+
+            {
+
+                output += resource.GetOutput();
+
+            }
+        }
+
+        TapText tapText = GetOrCreateTapText();
+        tapText.transform.SetParent(parent, false);
+        tapText.transform.position = tapPosition;
+
+
+        tapText.Text.text = $"+{output.ToString("0")}";
+        tapText.gameObject.SetActive(true);
+        CoinIcon.transform.localScale = Vector3.one * 1.75f;
+
+        AddGold(output);
+    }
+
+    private TapText GetOrCreateTapText()
+    {
+        TapText tapText = _tapTextPool.Find(t => !t.gameObject.activeSelf);
+        if(tapText == null)
+        {
+            tapText = Instantiate(TapTextPrefab).GetComponent<TapText>();
+            _tapTextPool.Add(tapText);
+        }
+
+        return tapText;
+    }
+
+    private void CheckResourceCost()
+    {
+        foreach(ResourceController resource in _activeResources)
+        {
+            bool isBuyable = false;
+            if (resource.IsUnlocked)
+            {
+                isBuyable = TotalGold >= resource.GetUpgradeCost();
+            }
+            else
+            {
+                isBuyable = TotalGold >= resource.GetUnlockCost();
+            }
+
+            resource.ResourceImage.sprite = ResourcesSprites[isBuyable ? 1 : 0];
+        }
+    }
+
+}
+
+[System.Serializable]// fungsi agar objek bisa di serialize dan value dapat di set di inspektor
+public struct ResourceConfig
+{
+    public string Name;
+    public double UnlockCost;
+    public double UpgradeCost;
+    public double Output;
+}
